86516544cef5cf7d5a9aa8b23cd84912df6cadf9
Colleen <colleen@zighra.com>
2018-07-03 08:05:13 -0400

    finished grid search on random forest

--- src/grid_search.py
+++ src/grid_search.py
@@ -16,7 +16,7 @@ def grid_search():
      train_data = pickle.load(f)
      f.close()
      
-     n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+     n_estimators = [200, 400, 600, 800] #[int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
      
      max_features = ['auto', 'sqrt']
      
@@ -25,23 +25,27 @@ def grid_search():
      
      min_samples_split = [2, 5, 10]
      
-     min_samples_leaf = [1, 2, 4]
+     min_samples_leaf = [5, 10, 50, 100]
      
      bootstrap = [True, False]
      
-     random_grid = {'n_estimators': n_estimators,
-                    'max_features': max_features,
+     random_grid = {'max_features': max_features,
                     'min_samples_leaf': min_samples_leaf}
      
-     random_grid = {'n_estimators': [n_estimators[0]]}
+     #random_grid = {'n_estimators': [n_estimators[-1]]}
      
      
      
-     rf = RandomForestRegressor(n_estimators = 200, max_features = 'auto', min_samples_leaf = 1,
+     rf = RandomForestRegressor(n_estimators = 500, max_features = 'auto', min_samples_leaf = 1,
                               n_jobs = -1)
+      
      '''
-     df = pca_train_data.iloc[range(pca_train_data.shape[0] / 3),:]
-     y = train_data['target'].iloc[range(pca_train_data.shape[0] / 3)]
+     df = pca_train_data.iloc[range(pca_train_data.shape[0] * 2 / 3),:]
+     y = train_data['target'].iloc[range(pca_train_data.shape[0] * 2 / 3)]
+     
+     #df = pca_train_data.iloc[range(2000),:]
+     #df = np.random.random(df.shape)
+     #y = train_data['target'].iloc[range(2000)]
      
      t0 = time.time()
      rf.fit(df, y)
@@ -54,18 +58,23 @@ def grid_search():
      #rf = RandomForestRegressor()
      #rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, 
      #                               n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+     
+
      grid_search = GridSearchCV(estimator = rf, param_grid = random_grid, 
-                               cv = 3, n_jobs = 1, verbose = 2)
+                               cv = 3, n_jobs = -1, verbose = 2,
+                               return_train_score = True,
+                               scoring = 'neg_mean_squared_error')
      
      #rand_mat = np.random.random(pca_train_data.shape)
      t0 = time.time()
      grid_search.fit(pca_train_data, train_data['target'])
      t1 = time.time()
      print t1 - t0
+
      
-     #f = open('grid_search_res.p', 'w')
-     #pickle.dump(grid_search, f)
-     #f.close()
+     f = open('grid_search_res2.p', 'w')
+     pickle.dump(grid_search, f)
+     f.close()
      
 
 if __name__ == '__main__':
