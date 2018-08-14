9191e8161622d6e709789cf4ae227cb14d819900
Colleen <colleen@zighra.com>
2018-08-14 08:02:41 -0400

    Implemented random grid search on RF

--- src/grid_search.py
+++ src/grid_search.py
@@ -3,21 +3,21 @@
 import pickle
 import time
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 
-def grid_search():
+def random_grid_search():
 
-     f = open('../data/proc_train_data.p', 'r')
-     pca_train_data = pickle.load(f)
+     f = open('C:\Users\Colleen\Documents\Kaggle_Santander_Value_Pred\data\pca_train_data.csv', 'r')
+     pca_train_data = pd.read_csv(f).iloc[:,1:]
      f.close()
      
-     f = open('../data/train_data.p', 'r')
-     train_data = pickle.load(f)
+     f = open('C:\Users\Colleen\Documents\Kaggle_Santander_Value_Pred\data\pca_y_train_data.csv', 'r')
+     y = pd.read_csv(f)['target']
      f.close()
      
-     n_estimators = [200, 400, 600, 800] #[int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-     
+
      max_features = ['auto', 'sqrt']
      
      max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
@@ -25,41 +25,30 @@ def grid_search():
      
      min_samples_split = [2, 5, 10]
      
-     min_samples_leaf = [5, 10, 50, 100]
+     min_samples_leaf = [1, 2, 4]
      
      bootstrap = [True, False]
-     
+
      random_grid = {'max_features': max_features,
-                    'min_samples_leaf': min_samples_leaf}
-     
-     #random_grid = {'n_estimators': [n_estimators[-1]]}
-     
-     
-     
-     rf = RandomForestRegressor(n_estimators = 500, max_features = 'auto', min_samples_leaf = 1,
-                              n_jobs = -1)
-      
-     '''
-     df = pca_train_data.iloc[range(pca_train_data.shape[0] * 2 / 3),:]
-     y = train_data['target'].iloc[range(pca_train_data.shape[0] * 2 / 3)]
-     
-     #df = pca_train_data.iloc[range(2000),:]
-     #df = np.random.random(df.shape)
-     #y = train_data['target'].iloc[range(2000)]
-     
-     t0 = time.time()
-     rf.fit(df, y)
-     t1 = time.time()
-     print t1 - t0
-     '''
-     
-     
-     
-     #rf = RandomForestRegressor()
-     #rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, 
-     #                               n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
      
 
+     rf = RandomForestRegressor(n_estimators = 200, n_jobs = -1)
+
+     rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, 
+                                   n_iter = 100, cv = 3, verbose=2,
+                                   random_state=42, n_jobs = -1)
+     
+     rf_random.fit(pca_train_data, y)
+     
+     f = open('C:\Users\Colleen\Documents\Kaggle_Santander_Value_Pred\data\rf_random.p', 'w')
+     pickle.dump(rf_random, f)
+     f.close()
+
+     '''
      grid_search = GridSearchCV(estimator = rf, param_grid = random_grid, 
                                cv = 3, n_jobs = -1, verbose = 2,
                                return_train_score = True,
@@ -75,7 +64,8 @@ def grid_search():
      f = open('grid_search_res2.p', 'w')
      pickle.dump(grid_search, f)
      f.close()
+     '''
      
 
 if __name__ == '__main__':
-     grid_search()
+     random_grid_search()
